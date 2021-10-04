e4f311530f7627e180dc494acd837d85e658c88f
ANKUBOSE <46471379+bosankus@users.noreply.github.com>
2021-10-05 01:34:07 +0530

    Created repository & implemented use cases.

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,28 @@
+package com.plcoding.cryptocurrencyappyt.domain.use_case.get_coins
+
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoin
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val coinRepository: CoinRepository
+) {
+
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = coinRepository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An expected error have occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check internet."))
+        }
+    }
+}
