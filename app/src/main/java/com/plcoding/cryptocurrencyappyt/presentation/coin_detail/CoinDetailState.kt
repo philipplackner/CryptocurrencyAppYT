9c95ea80c53551de8eafd37fe57e6cc3b06d680a
ANKUBOSE <46471379+bosankus@users.noreply.github.com>
2021-10-05 13:21:10 +0530

    Completed ViewModels setup

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_detail/CoinDetailState.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_detail
+
+import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail? = null,
+    val error: String = ""
+)
